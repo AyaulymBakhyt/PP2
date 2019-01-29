7e4549c15e5cce41c1a07561ed0b92ef172da08d
AyaulymBakhyt <luxuryteenager@gmail.com>
2019-01-29 21:56:15 +0300

    upd

---
+++ Week1/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        class Student
+        {
+            string name, ID;
+            int year = 0;
+        public Student(string name,string ID)
+            {
+                this.name = name;
+                this.ID = ID;
+            }
+            public void toPrint()
+            {
+                Console.WriteLine("Name : "+ name);
+                Console.WriteLine("ID : " + ID);
+            }
+            public void increment()
+            {
+                year++;
+                Console.WriteLine("Year of study now is : {0}", year);
+            }
+        }
+        static void Main(string[] args)
+
+        {
+            Student s1 = new Student("Ayaulym", "18BD110767");
+            s1.toPrint();
+            s1.increment();
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string amount = Console.ReadLine();
+            string line = Console.ReadLine();
+
+            int n = int.Parse(amount);
+            string[] doubnum = line.Split();
+
+            for (int i = 0; i < doubnum.Length; ++i)
+            {
+                int x = Convert.ToInt32(doubnum[i]);
+                for (int j = 0; j < 2; ++j)
+                {
+                    Console.Write(x + " ");
+                }
+
+            }
+        }
+    }
+}
